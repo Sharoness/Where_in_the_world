a95779499e622269c60a95b45f40a5e79402db8f
Concetta Iuso <conciuso@emeal.nttdata.com>
2024-02-22 17:04:53 +0000

    amended main.tsx

--- src/main.tsx
+++ src/main.tsx
@@ -3,14 +3,14 @@ import ReactDOM from 'react-dom/client';
 import App from './App.tsx';
 import './index.css';
 import CountryDetails from './CountryDetails.tsx';
+
+
 import {
   createBrowserRouter,
   RouterProvider,
 } from "react-router-dom";
 import Layout from './Layout.tsx';
 
-const countriesContext = createContext();
-const [countries, setCountries] = useState([]);
 const router = createBrowserRouter([
   {
     path: "/",
@@ -24,13 +24,6 @@ const router = createBrowserRouter([
 
 ReactDOM.createRoot(document.getElementById('root')!).render(
   <React.StrictMode>
-    <App></App>
-    <countriesContext.Provider value={countries}>
-        <RouterProvider router={router} />
-    </countriesContext.Provider> 
+    <RouterProvider router={router} />
   </React.StrictMode>,
 )
-function createContext() {
-  throw new Error('Function not implemented.');
-}
-
