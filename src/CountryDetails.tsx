7033addf86024e82a9f6597c19bbf8bdba28e9b2
Concetta Iuso <conciuso@emeal.nttdata.com>
2024-04-11 17:05:59 +0100

    added margin bottom and top to the back button

--- src/CountryDetails.tsx
+++ src/CountryDetails.tsx
@@ -1,114 +1,139 @@
-
-import { useLocation } from 'react-router-dom';
+import { useLocation } from "react-router-dom";
 import { useEffect, useState } from "react";
-import { useNavigate } from 'react-router-dom';
-import { ICountry, CurrencyName } from './interface';
+import { useNavigate } from "react-router-dom";
+import { ICountry, CurrencyName } from "./interface";
 import "./App.css";
-import { faArrowAltCircleLeft, faArrowLeft } from '@fortawesome/free-solid-svg-icons';
-import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
-const CountryDetails = ({defaultCountries} : {defaultCountries: ICountry[]}) => {
-    const [country, setCountry] = useState<ICountry>();
-    const [borders, setBorders] = useState([]);
-    const [countryPath, setCountryPath] = useState("")
-    const location = useLocation();
-    const navigate = useNavigate();
-    
-    const countryName = decodeURI(location.pathname.replace('/details/', ""))
+import {
+  faArrowAltCircleLeft,
+  faArrowLeft,
+} from "@fortawesome/free-solid-svg-icons";
+import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
+const CountryDetails = ({
+  defaultCountries,
+}: {
+  defaultCountries: ICountry[];
+}) => {
+  const [country, setCountry] = useState<ICountry>();
+  const [borders, setBorders] = useState([]);
+  const [countryPath, setCountryPath] = useState("");
+  const location = useLocation();
+  const navigate = useNavigate();
 
-    useEffect(() => {
-        fetchcountry();
-    }, [countryPath]);
-    
-    
-    useEffect(() => {
-    }, [defaultCountries]);
-    
-    
-    async function fetchcountry() {
-           
-       const url = `https://restcountries.com/v3.1/name/${countryName}`
-        
-        const response = await fetch(url); 
-        const country = await response.json();
-        setCountry(country[0]);
-        
-        const fullBordercountryName = country[0].borders?.map((countryBorder:string)=>{
-           const cioc =  defaultCountries.filter((defCountries)=> defCountries.cca3 === countryBorder)[0]
-           const fullCountryName = cioc?.name!.common;
-           return fullCountryName
-        })
-        setBorders(fullBordercountryName);
-    }
+  const countryName = decodeURI(location.pathname.replace("/details/", ""));
 
+  useEffect(() => {
+    fetchcountry();
+  }, [countryPath]);
 
-    const handleClick = () => {
-        navigate(`/`);
-    }
+  useEffect(() => {}, [defaultCountries]);
 
-    const handleBottomBorderClick = (country:string) => {
-        navigate(`/details/${country}`,  {state: {type: "code"}});
-        setCountryPath(country)
-    }
+  async function fetchcountry() {
+    const url = `https://restcountries.com/v3.1/name/${countryName}`;
 
+    const response = await fetch(url);
+    const country = await response.json();
+    setCountry(country[0]);
 
-    const currency = (currenciesArray: CurrencyName[] ) => {
-        const currenciesName = currenciesArray.map((item) => item.name).toString();
-        return currenciesName;
-    }
-
-    return (
-        <div className='container'>
-
-        <button className= "borderButton" onClick={handleClick}> <FontAwesomeIcon className='icon-padding' icon={faArrowLeft}/> Back</button>
-            {country && (
-                <div>
-                    <div>
-                        <img className="flagImage" src={country.flags!.svg}></img>
-                    </div>
-                    <div>
-                        <span className="title">{country.name!.common}</span>
-                        <div>
-                            <span>Native Name: </span>
-                            {country.name!.common}
-                        </div>
-                        <div>
-                            <span>Population: </span>
-                            {country.population}
-                        </div>
-                        <div>
-                            <span>Region: </span>
-                            {country.region}
-                        </div>
-                        <div>
-                            <span>Sub Region: </span>
-                            {country.subregion}
-                        </div>
-                        <div>
-                            <span>Capital: </span>
-                            {country.capital}
-                        </div> <br />
-                        <div>
-                            <span>Top level domain: </span>
-                            {country.tld![0]}
-                        </div>
-                        <div>
-                            <span>Currencies:</span>
-                            {currency(Object.values(country.currencies!))}
-                        </div>
-
-                        <div>
-                            <span>Languages: </span>
-                            {Object.values(country.languages!)}
-                        </div>
-                    </div>
-                </div>
-            )}
-            
-            {borders ? <><div>Border countries</div> {borders.map((value, index) => {
-                return (<button key={index} onClick={() => handleBottomBorderClick(value)} className='borderButton'>{value}</button>)
-            })}</> : null}
-        </div>
+    const fullBordercountryName = country[0].borders?.map(
+      (countryBorder: string) => {
+        const cioc = defaultCountries.filter(
+          (defCountries) => defCountries.cca3 === countryBorder
+        )[0];
+        const fullCountryName = cioc?.name!.common;
+        return fullCountryName;
+      }
     );
+    setBorders(fullBordercountryName);
+  }
+
+  const handleClick = () => {
+    navigate(`/`);
+  };
+
+  const handleBottomBorderClick = (country: string) => {
+    navigate(`/details/${country}`, { state: { type: "code" } });
+    setCountryPath(country);
+  };
+
+  const currency = (currenciesArray: CurrencyName[]) => {
+    const currenciesName = currenciesArray.map((item) => item.name).toString();
+    return currenciesName;
+  };
+
+  return (
+    <div className="container">
+      <button className="border-button" onClick={handleClick}>
+        {" "}
+        <FontAwesomeIcon className="icon-padding" icon={faArrowLeft} /> Back
+      </button>
+      {country && (
+        <div>
+          <div>
+            <img className="flagImage" src={country.flags!.svg}></img>
+          </div>
+          <div>
+            <div>
+              <span className="title">{country.name!.common}</span>
+            </div>
+
+            <div>
+              <span>Native Name: </span>
+              {country.name!.common}
+            </div>
+            <div>
+              <span>Population: </span>
+              {country.population}
+            </div>
+            <div>
+              <span>Region: </span>
+              {country.region}
+            </div>
+            <div>
+              <span>Sub Region: </span>
+              {country.subregion}
+            </div>
+            <div>
+              <span>Capital: </span>
+              {country.capital}
+            </div>
+          </div>
+
+          <div>
+            <div>
+              <span>Top level domain: </span>
+              {country.tld![0]}
+            </div>
+            <div>
+              <span>Currencies:</span>
+              {currency(Object.values(country.currencies!))}
+            </div>
+
+            <div>
+              <span>Languages: </span>
+              {Object.values(country.languages!)}
+            </div>
+          </div>
+        </div>
+      )}
+
+      {borders ? (
+        <>
+          <div>Border countries</div>{" "}
+          {borders.map((value, index) => {
+            return (
+              <button
+                key={index}
+                onClick={() => handleBottomBorderClick(value)}
+                className="border-button"
+              >
+                {value}
+              </button>
+            );
+          })}
+        </>
+      ) : null}
+    </div>
+  );
 };
 
 export default CountryDetails;
